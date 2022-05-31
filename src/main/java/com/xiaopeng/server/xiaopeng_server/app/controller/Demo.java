05ca99894723f9ece32c860fd9aae79f4eef4d4d
xiaopeng <os.wanzhp@ffcs.cn>
2022-05-31 15:23:01 +0800

    20220531

--- src/main/java/com/xiaopeng/server/xiaopeng_server/app/controller/Demo.java
+++ src/main/java/com/xiaopeng/server/xiaopeng_server/app/controller/Demo.java
@@ -3,7 +3,7 @@ package com.xiaopeng.server.xiaopeng_server.app.controller;
 import com.xiaopeng.server.xiaopeng_server.app.bean.ResultBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/demo")
 public class Demo {
-    @GetMapping("/hellowWord")
-    public String testxiaopeng(Model model){
+    @PostMapping("/hellowWord")
+    public ResultBean testxiaopeng(Model model){
         model.addAttribute("hi", "小鹏主机的第一个程序!");
-//        return new ResultBean(ResultBean.SUCCESS,"Hello,小鹏!");
-        return "Hello,小鹏!";
+        return new ResultBean(ResultBean.SUCCESS,"Hello,小鹏!");
     }
 }
 
