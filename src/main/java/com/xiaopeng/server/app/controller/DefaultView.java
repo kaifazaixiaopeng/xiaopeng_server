849aa0a33d5572d8efc0df277ede93a3c5f47127
wzp <xiaopengaijava@foxmail.com>
2023-05-15 17:28:33 +0800

    20230515更新

--- src/main/java/com/xiaopeng/server/app/controller/DefaultView.java
+++ src/main/java/com/xiaopeng/server/app/controller/DefaultView.java
@@ -1,19 +1,19 @@
-package com.xiaopeng.server.app.controller;
-
-/**
- * @Auto:BUGPeng
- * @Date:2022/11/9 23:36
- * @ClassName:DefaultView
- * @Remark:
- */
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-/**
- * 设置默认页面
- */
+//package com.xiaopeng.server.app.controller;
+//
+///**
+// * @Auto:BUGPeng
+// * @Date:2022/11/9 23:36
+// * @ClassName:DefaultView
+// * @Remark:
+// */
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.Ordered;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * 设置默认页面
+// */
 //@Configuration
 //public class DefaultView extends WebMvcConfigurerAdapter {
 //    @Override
