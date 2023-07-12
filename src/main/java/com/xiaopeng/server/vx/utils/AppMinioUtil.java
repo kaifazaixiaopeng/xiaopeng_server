582a8e770088095909b4042d40f6ba230b50ff8d
wzp <xiaopengaijava@foxmail.com>
2023-07-12 17:01:15 +0800

    20230711更新

--- src/main/java/com/xiaopeng/server/vx/utils/AppMinioUtil.java
+++ src/main/java/com/xiaopeng/server/vx/utils/AppMinioUtil.java
@@ -3,6 +3,7 @@ package com.xiaopeng.server.vx.utils;
 
 import io.minio.*;
 import io.minio.messages.Bucket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FastByteArrayOutputStream;
 
@@ -16,7 +17,7 @@ import java.util.List;
 @Component
 public class AppMinioUtil {
 
-    @Resource
+    @Autowired
     private MinioClient minioClient;
 
 
